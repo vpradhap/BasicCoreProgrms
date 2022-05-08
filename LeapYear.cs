9c2773ffda3e18e49ed563ced61c18ddc13a9fb5
vpradhap <pradhapvenkat@gmail.com>
2022-05-08 12:13:36 +0530

    Added : Leap Year program

---
+++ LeapYear.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgrms
+{
+    public class LeapYear
+    {
+        public static void Leap()
+        {
+            Console.Write("Enter Four Digit year : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            //Condition to check 4 digit number or not
+            if (year >= 1000 && year <= 9999)
+            {
+                //Leap Year Logic
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                {
+                    Console.WriteLine(year+" is Leap Year");
+                }
+                else
+                {
+                    Console.WriteLine(year+" is not Leap Year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
 ﻿using BasicCoreProgrms;
 
+//Options for which basic programs to pick
+Console.WriteLine("Pick Which program You Want to Perform");
+Console.WriteLine("1 - Flipcoin Program");
+Console.WriteLine("2 - LeapYear Program");
+Console.Write("Your Pick : ");
 
-FlipCoin.Flip();
+int pick = Convert.ToInt32(Console.ReadLine());
+switch (pick)
+{
+    case 1:
+        FlipCoin.Flip();
+        break;
+    case 2:
+        LeapYear.Leap();
+        break;
+    default:
+        Console.WriteLine("Invalid Input");
+        break;
+}
