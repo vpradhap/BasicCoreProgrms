d306e7233d244d4579215b6d9a39e858f2a12541
vpradhap <pradhapvenkat@gmail.com>
2022-05-08 12:51:31 +0530

    Added : Power of 2 Program

---
+++ PowerOf2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgrms
+{
+    public class PowerOf2
+    {
+        public static void Power()
+        {
+            int i; //Declaring local variable
+            Console.Write("Enter the value for N : ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            //checking the given condition and perform loop for user input
+            if (N >= 0 && N < 31)
+            {
+                for ( i = 0; i <= N; i++)
+                {
+                    Console.WriteLine("The value of 2 power " + i + " : " + Math.Pow(2, i));
+                }
+            }
+            else
+            {
+                Console.WriteLine("2^31 Overflows an integer");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
 Console.WriteLine("Pick Which program You Want to Perform");
 Console.WriteLine("1 - Flipcoin Program");
 Console.WriteLine("2 - LeapYear Program");
+Console.WriteLine("3 - Powerof2 Program");
 Console.Write("Your Pick : ");
 
 int pick = Convert.ToInt32(Console.ReadLine());
@@ -14,6 +15,9 @@ switch (pick)
         break;
     case 2:
         LeapYear.Leap();
+        break;
+    case 3:
+        PowerOf2.Power();
         break;
     default:
         Console.WriteLine("Invalid Input");
